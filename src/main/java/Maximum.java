c419efbd7d67b9c76625ef5e828f664fdcaf7503
praveen8790 <praveen8978470998rock@gmail.com>
2021-06-03 23:12:59 +0530

    REFACTORED to implement generic class and extended comparable and initialized  3 instance variables

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,11 +1,22 @@
 public class Maximum<T extends Comparable<T>> {
-    public <T extends Comparable<T>> T findMaximum(T a, T b, T c){
+    public T a;
+    public T b;
+    public T c;
+
+    public Maximum(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public <T extends Comparable<T>> T testMaximum(){
+
         if(b.compareTo(a)>0 && b.compareTo(c)>0)
-          return b;
+          return (T) b;
         else if(c.compareTo(a)>0 && c.compareTo(b)>0)
-            return c;
+            return (T) c;
         else
-            return a;
+            return (T) a;
     }
 
 }
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -4,56 +4,56 @@ import org.junit.Test;
 public class MaximumTest {
     @Test
     public void integer1() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(6,4,5);
         Integer expected =6;
-        Assert.assertEquals(expected, max.findMaximum(6,4,5));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void integer2() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(4,6,5);
         Integer expected =6;
-        Assert.assertEquals(expected, max.findMaximum(4,6,5));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void integer3() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(5,6,5);
         Integer expected =6;
-        Assert.assertEquals(expected, max.findMaximum(5,6,5));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void float1() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(6.7f,1.6f,5.5f);
         Float expected =6.7f;
-        Assert.assertEquals(expected, max.findMaximum(6.7f,1.6f,5.5f));
+        Assert.assertEquals(expected, max.testMaximum());
     }@Test
     public void float2() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(5.2f,6.7f,5.5f);
         Float expected =6.7f;
-        Assert.assertEquals(expected, max.findMaximum(5.2f,6.7f,5.5f));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void float3() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(5.5f,1.6f,6.7f);
         Float expected =6.7f;
-        Assert.assertEquals(expected, max.findMaximum(5.5f,1.6f,6.7f));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void string1() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum("how are you","hii","bye");
         String expected ="how are you";
-        Assert.assertEquals(expected, max.findMaximum("how are you","hii","bye"));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void string2() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum(" you","how are you","bye");
         String expected ="how are you";
-        Assert.assertEquals(expected, max.findMaximum(" you","how are you","bye"));
+        Assert.assertEquals(expected, max.testMaximum());
     }
     @Test
     public void string3() {
-        Maximum max = new Maximum();
+        Maximum max = new Maximum("how are u","hii","how are you");
         String expected ="how are you";
-        Assert.assertEquals(expected, max.findMaximum("how are u","hii","how are you"));
+        Assert.assertEquals(expected, max.testMaximum());
     }
 
 
