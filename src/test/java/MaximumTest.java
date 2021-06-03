143eef5b00d1db565dadc5bad7b855a29c26cffd
praveen8790 <praveen8978470998rock@gmail.com>
2021-06-03 22:33:47 +0530

    ADDED method to find maximum in Float

--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -20,4 +20,22 @@ public class MaximumTest {
         Integer expected =6;
         Assert.assertEquals(expected, max.findMaximum(5,6,5));
     }
+    @Test
+    public void float1() {
+        Maximum max = new Maximum();
+        Float expected =6.7f;
+        Assert.assertEquals(expected, max.findMaximum(6.7f,1.6f,5.5f));
+    }@Test
+    public void float2() {
+        Maximum max = new Maximum();
+        Float expected =6.7f;
+        Assert.assertEquals(expected, max.findMaximum(5.2f,6.7f,5.5f));
+    }
+    @Test
+    public void float3() {
+        Maximum max = new Maximum();
+        Float expected =6.7f;
+        Assert.assertEquals(expected, max.findMaximum(5.5f,1.6f,6.7f));
+    }
+
 }
