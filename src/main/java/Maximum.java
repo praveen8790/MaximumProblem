f06829df1a1a81e118e398517c78a0ceff4145ba
praveen8790 <praveen8978470998rock@gmail.com>
2021-06-03 23:00:06 +0530

    REFACTORED to implement generic method and extended comparable

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,23 +1,7 @@
-public class Maximum {
-    public Integer findMaximum(Integer a, Integer b, Integer c){
+public class Maximum<T extends Comparable<T>> {
+    public <T extends Comparable<T>> T findMaximum(T a, T b, T c){
         if(b.compareTo(a)>0 && b.compareTo(c)>0)
           return b;
-        else if(c.compareTo(a)>0 && c.compareTo(b)>0)
-            return c;
-        else
-            return a;
-    }
-    public Float findMaximum(Float a, Float b, Float c){
-        if(b.compareTo(a)>0 && b.compareTo(c)>0)
-            return b;
-        else if(c.compareTo(a)>0 && c.compareTo(b)>0)
-            return c;
-        else
-            return a;
-    }
-    public String findMaximum(String a, String b, String c){
-        if(b.compareTo(a)>0 && b.compareTo(c)>0)
-            return b;
         else if(c.compareTo(a)>0 && c.compareTo(b)>0)
             return c;
         else
